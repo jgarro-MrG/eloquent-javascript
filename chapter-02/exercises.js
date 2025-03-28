5068e66e0b584316058044dbfeee119bf8c20e0b
Jorge Garro <jorge.garroelizondo@jpschools.org>
2025-03-28 22:35:10 +0000

    fizzBuzz done

--- chapter-02/exercises.js
+++ chapter-02/exercises.js
@@ -48,7 +48,17 @@ on the number:
 */
 
 function fizzBuzz(start, end) {
-  
+  for (let i = start; i <= end; i++) {  
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } else if (i % 3 === 0 && i % 5 !== 0) {
+      console.log('fizz');
+    } else if (i % 3 !== 0 && i % 5 === 0) {
+      console.log('buzz');
+    } else {
+      console.log(i);
+    }
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
